748bcbda913a80707453c7e300d869e82b167c7c
yoshihiro asada <yoshihiro0921@gmail.com>
2015-07-12 18:43:29 +0900

    Add function of session create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      sign_in user
+      redirect_to root_url, notice: 'ログインしました'
+    else
+      flash.alert = 'ログイン名またはパスワードが正しくありません。'
+      render 'new'
+    end
   end
 
   def destroy
