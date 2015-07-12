75f1e237d655e6487645dfad9205aa97f0fb2410
yoshihiro asada <yoshihiro0921@gmail.com>
2015-07-12 16:41:19 +0900

    rails g scaffold user

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :email, :password_digest, :auto_login_token, :created_at, :updated_at
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+email "MyString"
+password_digest "MyString"
+auto_login_token "MyString"
+  end
+
+end
