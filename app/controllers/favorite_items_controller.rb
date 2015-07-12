3927380aa5916db5e04d8a4891ace5f4f7a2e688
yoshihiro asada <yoshihiro0921@gmail.com>
2015-07-13 06:49:12 +0900

    Add function of creating and destroying on favorite_items

--- app/controllers/favorite_items_controller.rb
+++ app/controllers/favorite_items_controller.rb
@@ -3,8 +3,22 @@ class FavoriteItemsController < ApplicationController
   end
 
   def create
+    @favorite = FavoriteItem.find_or_create_by(item: params[:item], user_id: current_user.id) if current_user
+    cookies.permanent[:item] = params[:item]
+    if @favorite.save
+      render layout: nil,  notice: "お気に入りに登録しました"
+    else
+      render layout: nil, alert: "この写真はお気に入りに登録できません"
+    end
   end
 
   def destroy
+    if current_user
+      @favorite = current_user.favorite_items.find_by(params[:item])
+    else
+      @favorite = cookies[:item]
+    end
+    @favorite.destroy
+    cookies.delete :item
   end
 end
